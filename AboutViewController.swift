6dd87d1db020ab1afd6dcf7036b55f6fcc3e8328
MrCesar107 <cesar.rodriguez.lara54@gmail.com>
2019-06-18 08:17:36 -0500

    Se agrego la funcionalidad al boton start over y tambien se creo la vista de ayuda del juego

---
+++ AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Cesar Rodriguez on 6/18/19.
+//  Copyright © 2019 Cesar Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func close() {
+    dismiss(animated: true, completion: nil)
+  }
+}
--- ViewController.swift
+++ ViewController.swift
@@ -10,20 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentValue: Int = 50
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 1
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewGame()
     }
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
         
-        let alert = UIAlertController(title: "Hello World", message: message,
+        let title: String
+        
+        if difference == 0 {
+            title = "Pefect!"
+            points += 100
+        } else if difference < 5 {
+            title = "Almost there!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close!"
+        }
+        
+        let message = "You scored \(points) points"
+        
+        score += points
+        
+        let alert = UIAlertController(title: title, message: message,
                                       preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: {
+            action in
+            self.startNewRound()
+        })
         
         alert.addAction(action)
         
@@ -33,5 +67,25 @@ class ViewController: UIViewController {
     @IBAction func sliderMoved(_ slider: UISlider) {
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
+    }
+    
+    @IBAction func startNewGame() {
+        round = 1
+        score = 0
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+        round += 1
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 }
